b756b941e626b6cdd018474822214b9d4cc0f8e6
Patryk Gaszczyk <patryk.gaszczyk@gmail.com>
2023-11-07 14:25:49 +0100

    styling change

--- hub/static/hub/script.js
+++ hub/static/hub/script.js
@@ -20,7 +20,6 @@ document.addEventListener('scroll', function () {
   }
 });
 
-
 function sortTable(n) {
   var i,
     x,
@@ -173,38 +172,50 @@ function searchOutcome(clickedSymbol) {
         document.getElementById("news-href").href = `https://twitter.com/search?q=${zeroTd.textContent}%20coin&src=typed_query&f=top`
       }
     }
-      if (myChart != false) {myChart.destroy()}
-      var ctx = document.getElementById('myChart').getContext('2d');
-      myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: uploadTime,
-          datasets: [{
-            label: '# of users',
-            data: historyPrice,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)', 
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          maintainAspectRatio: false
+    if (myChart != false) {myChart.destroy()}
+    var ctx = document.getElementById('myChart').getContext('2d');
+    myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: uploadTime,
+        datasets: [{
+          label: "test",
+          data: historyPrice,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)', 
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false
         }
-      });
+      },
+        elements: {
+          point: {
+            radius: 0
+          }
+        }
+      }
+    });
     }
 
+    
+  
 
