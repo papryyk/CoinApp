1cf7fb7de195c1e001399c1291ad25ba8eedf1ba
papryyk <patryk.gaszczyk@gmail.com>
2023-12-29 23:05:52 +0100

    Serve staticfiles via web server

--- CoinApp/urls.py
+++ CoinApp/urls.py
@@ -23,4 +23,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("hub.urls"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
